9fbc650bd0752d913e095b272a9464933725be45
Jameskillr <amiteshjames00718@gmail.com>
2023-04-13 12:44:59 +0530

    Amitesh - img update

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,15 +6,20 @@ import HeadingComp from "../components/HeadingComp";
 const HomeScreen = () => {
   return (
     <div>
-      <main className="img">
+      <main className=" bg-[url('/public/images/hero.png')]">
         <NavbarComp />
         <HeroComp />
+
+        {/* Destination section ------------------------- */}
         <section className="bg-white">
           <HeadingComp
             number="01."
             text="Destination"
             heading="Find Your Perfect Destination With Vsitto"
           />
+          <section>
+            <div className="container flex flex-row gap-28 px-28">
+            
         </section>
         <section className="bg-black text-white">
           <HeadingComp
